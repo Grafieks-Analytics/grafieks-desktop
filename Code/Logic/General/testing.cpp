bc16623980c7384c0a25232671ef8d6e4e720655
Chilarai Mushahary <chilly5476@gmail.com>
2020-06-22 20:26:56 +0530

    Minor changes

--- Code/Logic/General/testing.cpp
+++ Code/Logic/General/testing.cpp
@@ -2,25 +2,19 @@
 
 Testing::Testing(QObject *parent) : QObject(parent)
 {
+
     this->google = new QOAuth2AuthorizationCodeFlow(this);
 
     // Set Scope or Permissions required from Google
     // List can be obtained from https://developers.google.com/identity/protocols/oauth2/scopes
 
-//    this->google->setScope("email https://www.googleapis.com/auth/drive.readonly");
-
+    this->google->setScope("email https://www.googleapis.com/auth/drive.readonly");
 
     connect(this->google, &QOAuth2AuthorizationCodeFlow::authorizeWithBrowser, [=](QUrl url) {
         QUrlQuery query(url);
-        QByteArray postData;
 
-        query.addQueryItem("force_reapprove", "true"); // Param required to get data everytime
-        query.addQueryItem("token_access_type", "offline"); // Needed for Refresh Token (as AccessToken expires shortly)
-        query.addQueryItem("response_type", "code");
-
-
-        postData = query.toString(QUrl::FullyEncoded).toUtf8();
-
+        query.addQueryItem("prompt", "consent");      // Param required to get data everytime
+        query.addQueryItem("access_type", "offline"); // Needed for Refresh Token (as AccessToken expires shortly)
         url.setQuery(query);
         QDesktopServices::openUrl(url);
     });
@@ -28,11 +22,10 @@ Testing::Testing(QObject *parent) : QObject(parent)
     // Here the parameters from Google JSON are filled up
     // Attached screenshot of JSON file and Google Console
 
-    this->google->setAuthorizationUrl(QUrl("https://www.dropbox.com/oauth2/authorize"));
-    this->google->setClientIdentifier("APP_ID");
-    this->google->setAccessTokenUrl(QUrl("https://api.dropboxapi.com/oauth2/token"));
-    this->google->setClientIdentifierSharedKey("APP_SECRET");
-
+    this->google->setAuthorizationUrl(QUrl("https://accounts.google.com/o/oauth2/auth"));
+    this->google->setClientIdentifier("MY_CLIENT_ID");
+    this->google->setAccessTokenUrl(QUrl("https://oauth2.googleapis.com/token"));
+    this->google->setClientIdentifierSharedKey("MY_SECRET_TOKEN");
 
     // In my case, I have hardcoded 5476
     // This is set in Redirect URI in Google Developers Console of the app
@@ -41,16 +34,17 @@ Testing::Testing(QObject *parent) : QObject(parent)
     auto replyHandler = new QOAuthHttpServerReplyHandler(5476, this);
     this->google->setReplyHandler(replyHandler);
 
-
-    connect(this->google, &QOAuth2AuthorizationCodeFlow::granted, [=](){
+    connect(this->google, &QOAuth2AuthorizationCodeFlow::granted, [=]() {
         qDebug() << __FUNCTION__ << __LINE__ << "Access Granted!";
 
-        auto reply = this->google->get(QUrl("https://www.googleapis.com/drive/v3/files?fields=files(*)"));
-        connect(reply, &QNetworkReply::finished, [reply](){
+        auto reply = this->google->get(QUrl("https://www.googleapis.com/drive/v3/files"));
+        connect(reply, &QNetworkReply::finished, [reply]() {
             qDebug() << "REQUEST FINISHED. Error? " << (reply->error() != QNetworkReply::NoError);
             qDebug() << reply->readAll();
         });
     });
+
+
 }
 
 
