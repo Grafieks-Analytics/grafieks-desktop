65a4ff009a5b831ed4a7ceb015edfc4c764a2b02
Chilarai Mushahary <chilly5476@gmail.com>
2020-08-12 19:36:33 +0530

    Tmp Filter functions model

--- Code/Logic/DataModeller/filterfunctionsmodel.cpp
+++ Code/Logic/DataModeller/filterfunctionsmodel.cpp
@@ -19,74 +19,104 @@ FilterFunctionsModel::FilterFunctionsModel(QObject *parent) : QObject(parent)
 float FilterFunctionsModel::mAverage(QVariantList numbers)
 {
 
+    Q_UNUSED(numbers);
+    return 0.0;
 }
 
 float FilterFunctionsModel::mMedian(QVariantList numbers)
 {
+    Q_UNUSED(numbers);
+    return 0.0;
 
 }
 
 float FilterFunctionsModel::mCount(QVariantList numbers)
 {
 
+    Q_UNUSED(numbers);
+    return 0.0;
 }
 
 float FilterFunctionsModel::mCountdistinct(QVariantList numbers)
 {
 
+    Q_UNUSED(numbers);
+    return 0.0;
 }
 
 float FilterFunctionsModel::mMinimum(QVariantList numbers)
 {
 
+    Q_UNUSED(numbers);
+    return 0.0;
 }
 
 float FilterFunctionsModel::mMaximum(QVariantList numbers)
 {
 
+    Q_UNUSED(numbers);
+    return 0.0;
 }
 
 float FilterFunctionsModel::mPercentile(QVariantList numbers)
 {
 
+    Q_UNUSED(numbers);
+    return 0.0;
 }
 
 float FilterFunctionsModel::mStddev(QVariantList numbers)
 {
 
+    Q_UNUSED(numbers);
+    return 0.0;
 }
 
 float FilterFunctionsModel::mStddevpop(QVariantList numbers)
 {
 
+    Q_UNUSED(numbers);
+    return 0.0;
 }
 
 float FilterFunctionsModel::mvariance(QVariantList numbers)
 {
 
+    Q_UNUSED(numbers);
+    return 0.0;
 }
 
 float FilterFunctionsModel::mVariancepop(QVariantList numbers)
 {
 
+    Q_UNUSED(numbers);
+    return 0.0;
 }
 
 int FilterFunctionsModel::sCount(QStringList strings)
 {
 
+    Q_UNUSED(strings);
+    return 0;
 }
 
 int FilterFunctionsModel::sCountdistinct(QStringList strings)
 {
 
+    Q_UNUSED(strings);
+    return 0;
 }
 
 int FilterFunctionsModel::sMinimum(QStringList strings)
 {
 
+    Q_UNUSED(strings);
+    return 0;
 }
 
 int FilterFunctionsModel::sMaximum(QStringList strings)
 {
 
+    Q_UNUSED(strings);
+    return 0;
 }
