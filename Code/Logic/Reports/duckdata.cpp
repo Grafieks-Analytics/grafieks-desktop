f33faea24bbf4fb7e49054b747d90682f971e89e
Deepanshu Vijay <deepanshuvijay123@gmail.com>
2021-02-27 17:48:49 +0530

    Fixed naming problems for csv files

--- Code/Logic/Reports/duckdata.cpp
+++ Code/Logic/Reports/duckdata.cpp
@@ -10,23 +10,6 @@ DuckData::DuckData(DuckCRUD *duckCRUD, QObject *parent)
     Q_UNUSED(parent);
     this->duckCRUD = duckCRUD;
 
-    QString db = Statics::currentDbName;
-
-    auto result = this->duckCRUD->con.Query("DESCRIBE " + db.toStdString());
-
-    int rows = result->collection.count;
-
-    for(int i = 0 ; i < rows; i++){
-
-        duckdb::Value fieldName = result->GetValue(0, i);
-        duckdb::Value fieldType = result->GetValue(1, i);
-
-        // string to qstring conversion
-        QString newFieldName = QString::fromStdString(fieldName.ToString());
-        QString newFilterType = QString::fromStdString(fieldType.ToString());
-
-        this->colInfo.insert(newFieldName, {i, newFilterType});
-    }
 }
 
 QString DuckData::getBarChartValues(QString xAxisColumn, QString yAxisColumn, QString yAxisCalculationType)
@@ -298,6 +281,10 @@ QString DuckData::getFunnelChartValues(QString xAxisColumn, QString yAxisColumn,
     QString strData = doc.toJson();
     return strData;
 }
+
+                                /*****************************************************
+                                **** Starting from here => charts are not working ***
+                                ******************************************************/
 
 QString DuckData::getRadarChartValues(QString xAxisColumn, QString yAxisColumn, QString yAxisCalculationType)
 {
