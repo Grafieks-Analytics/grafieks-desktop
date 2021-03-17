232f31054b9458b84891b73558947244574afe2c
Chilarai <chilly5476@gmail.com>
2021-03-17 19:52:29 +0530

    More singular charts

--- Code/Logic/Reports/chartsmodel.cpp
+++ Code/Logic/Reports/chartsmodel.cpp
@@ -192,17 +192,364 @@ QString ChartsModel::getFunnelChartValues(QString xAxisColumn, QString yAxisColu
 
 QString ChartsModel::getRadarChartValues(QString xAxisColumn, QString yAxisColumn, QString yAxisCalculationType)
 {
+    QJsonArray data;
+    QJsonArray axisArray;
+
+    // Fetch data here
+    int xKey = newChartHeader.key( xAxisColumn );
+    int yKey = newChartHeader.key( yAxisColumn );
+
+    int totalData = (*newChartData.value(xKey)).length();
+
+    for(int i = 0; i < totalData; i++){
+        QJsonArray axisData;
+        axisData.append((*newChartData.value(xKey)).at(i));
+        axisData.append((*newChartData.value(yKey)).at(i));
+        axisArray.append(axisData);
+    }
+
+
+    QJsonArray colData;
+    colData.append(axisArray);
+
+    QJsonArray columns;
+    columns.append(xAxisColumn);
+    columns.append(yAxisColumn);
+
+
+    data.append(colData);
+    data.append(columns);
+
+    QJsonDocument doc;
+    doc.setArray(data);
+
+    QString strData = doc.toJson();
+
+    return strData;
+}
+
+QString ChartsModel::getScatterChartValues(QString xAxisColumn, QString yAxisColumn, QString groupName)
+{
+    QJsonArray data;
+    QJsonArray axisArray;
+
+    // Fetch data here
+    int xKey = newChartHeader.key( xAxisColumn );
+    int yKey = newChartHeader.key( yAxisColumn );
+    int groupNameKey = newChartHeader.key( groupName );
+
+    int totalData = (*newChartData.value(xKey)).length();
+
+    for(int i = 0; i < totalData; i++){
+        QJsonArray axisData;
+        axisData.append((*newChartData.value(xKey)).at(i));
+        axisData.append((*newChartData.value(yKey)).at(i));
+        axisData.append((*newChartData.value(groupNameKey)).at(i));
+        axisArray.append(axisData);
+    }
+
+    qDebug() << axisArray << "Scatter";
+
+    QJsonArray colData;
+    colData.append(axisArray);
+
+    QJsonArray columns;
+    columns.append(xAxisColumn);
+    columns.append(yAxisColumn);
+
+    data.append(colData);
+    data.append(columns);
+
+    QJsonDocument doc;
+    doc.setArray(data);
+
+    QString strData = doc.toJson();
+
+    return strData;
+}
+
+QString ChartsModel::getHeatMapChartValues(QString xAxisColumn, QString yAxisColumn, QString groupName)
+{
+    QJsonArray data;
+    QJsonArray axisArray;
+
+    // Fetch data here
+    int xKey = newChartHeader.key( xAxisColumn );
+    int yKey = newChartHeader.key( yAxisColumn );
+    int groupNameKey = newChartHeader.key( groupName );
+
+    int totalData = (*newChartData.value(xKey)).length();
+
+    for(int i = 0; i < totalData; i++){
+        QJsonArray axisData;
+        axisData.append((*newChartData.value(groupNameKey)).at(i));
+        axisData.append((*newChartData.value(xKey)).at(i));
+        axisData.append((*newChartData.value(yKey)).at(i));
+        axisArray.append(axisData);
+    }
+
+    QJsonArray colData;
+    colData.append(axisArray);
+
+    QJsonArray columns;
+    columns.append(xAxisColumn);
+    columns.append(yAxisColumn);
+
+    data.append(colData);
+    data.append(columns);
+
+    QJsonDocument doc;
+    doc.setArray(data);
+
+    QString strData = doc.toJson();
+
+    return strData;
+}
+
+QString ChartsModel::getSunburstChartValues(QString xAxisColumn, QString yAxisColumn, QStringList groupNames)
+{
+
+    QJsonArray data;
+    QJsonArray axisArray;
+
+    // Fetch data here
+    int xKey = newChartHeader.key( xAxisColumn );
+    int yKey = newChartHeader.key( yAxisColumn );
+
+    // Group name operations
+    QVector<int> groupKeyValues;
+    int groupKeySize = groupNames.length();
+
+    for(int i = 0; i < groupKeySize; i++){
+        groupKeyValues.append(newChartHeader.key(groupNames.at(i)));
+    }
+
+    int totalData = (*newChartData.value(xKey)).length();
+
+    for(int i = 0; i < totalData; i++){
+        QJsonObject axisData;
+        axisData.insert("name", (*newChartData.value(xKey)).at(i));
+        axisData.insert("size", (*newChartData.value(yKey)).at(i));
+        axisArray.append(axisData);
+
+    }
+
+    QJsonObject colData;
+    colData.insert("name", groupNames.at(0));
+    colData.insert("children", axisArray);
+
+    QJsonArray columns;
+    columns.append(xAxisColumn);
+    columns.append(yAxisColumn);
+
+    data.append(colData);
+    data.append(columns);
+
+    QJsonDocument doc;
+    doc.setArray(data);
+
+    QString strData = doc.toJson();
+
+    return strData;
     return "";
 }
 
-QString ChartsModel::getScatterChartValues(QString xAxisColumn, QString yAxisColumn, QString xAxisCalculationType, QString yAxisCalculationType, QString groupedBy)
+QString ChartsModel::getWaterfallChartValues(QString xAxisColumn, QString yAxisColumn, QStringList groupNames)
+{
+    QJsonArray data;
+    QJsonArray axisArray;
+
+    // Fetch data here
+    int xKey = newChartHeader.key( xAxisColumn );
+    int yKey = newChartHeader.key( yAxisColumn );
+
+    int totalData = (*newChartData.value(xKey)).length();
+
+    for(int i = 0; i < totalData; i++){
+        QJsonArray axisData;
+        axisData.append((*newChartData.value(xKey)).at(i));
+        axisData.append((*newChartData.value(yKey)).at(i));
+        axisArray.append(axisData);
+    }
+
+    qDebug() << "WATERFALL" << axisArray;
+
+    QJsonArray colData;
+    colData.append(axisArray);
+
+    QJsonArray columns;
+    columns.append(xAxisColumn);
+    columns.append(yAxisColumn);
+
+
+    data.append(colData);
+    data.append(columns);
+
+    QJsonDocument doc;
+    doc.setArray(data);
+
+    QString strData = doc.toJson();
+
+    return strData;
+}
+
+QString ChartsModel::getGaugeChartValues(QString xAxisColumn, QString yAxisColumn, QStringList groupNames)
 {
     return "";
 }
 
-QString ChartsModel::getHeatMapChartValues(QString xAxisColumn, QString yAxisColumn, QString groupedBy)
+QString ChartsModel::getSankeyChartValues(QString xAxisColumn, QString yAxisColumn, QStringList groupNames)
+{
+    QJsonArray data;
+    QJsonArray axisArray;
+
+    // Fetch data here
+    int xKey = newChartHeader.key( xAxisColumn );
+    int yKey = newChartHeader.key( yAxisColumn );
+
+    int totalData = (*newChartData.value(xKey)).length();
+
+    for(int i = 0; i < totalData; i++){
+        QJsonObject axisData;
+        axisData.insert("node", (*newChartData.value(xKey)).at(i));
+        axisData.insert("name", (*newChartData.value(yKey)).at(i));
+        axisArray.append(axisData);
+    }
+
+    qDebug() << "SANKEY" << axisArray;
+
+    QJsonArray colData;
+    colData.append(axisArray);
+
+    QJsonArray columns;
+    columns.append(xAxisColumn);
+    columns.append(yAxisColumn);
+
+
+    data.append(colData);
+    data.append(columns);
+
+    QJsonDocument doc;
+    doc.setArray(data);
+
+    QString strData = doc.toJson();
+
+    return strData;
+}
+
+QString ChartsModel::getTreeChartValues(QString xAxisColumn, QString yAxisColumn, QStringList groupNames)
+{
+
+    return "";
+}
+
+QString ChartsModel::getTreeMapChartValues(QString xAxisColumn, QString yAxisColumn, QStringList groupNames)
+{
+    QJsonArray data;
+    QJsonArray axisArray;
+
+    // Fetch data here
+    int xKey = newChartHeader.key( xAxisColumn );
+    int yKey = newChartHeader.key( yAxisColumn );
+
+    // Group name operations
+    QVector<int> groupKeyValues;
+    int groupKeySize = groupNames.length();
+
+    for(int i = 0; i < groupKeySize; i++){
+        groupKeyValues.append(newChartHeader.key(groupNames.at(i)));
+    }
+
+    int totalData = (*newChartData.value(xKey)).length();
+
+    for(int i = 0; i < totalData; i++){
+        QJsonObject axisData;
+        axisData.insert("name", (*newChartData.value(xKey)).at(i));
+        axisData.insert("size", (*newChartData.value(yKey)).at(i));
+        axisArray.append(axisData);
+
+    }
+
+    QJsonObject colData;
+    colData.insert("name", groupNames.at(0));
+    colData.insert("children", axisArray);
+
+    QJsonArray columns;
+    columns.append(xAxisColumn);
+    columns.append(yAxisColumn);
+
+    data.append(colData);
+    data.append(columns);
+
+    QJsonDocument doc;
+    doc.setArray(data);
+
+    QString strData = doc.toJson();
+
+    return strData;
+    return "";
+}
+
+QString ChartsModel::getKPIChartValues(QString xAxisColumn, QString yAxisColumn, QStringList groupNames)
 {
     return "";
+}
+
+QString ChartsModel::getTableChartValues(QString xAxisColumn, QString yAxisColumn, QStringList groupNames)
+{
+    return "";
+}
+
+QString ChartsModel::getPivotChartValues(QString xAxisColumn, QString yAxisColumn, QStringList groupNames)
+{
+    return "";
+}
+
+QString ChartsModel::getParentChildValues(QString xAxisColumn, QString yAxisColumn, QStringList groupNames)
+{
+
+    QJsonArray data;
+    QJsonArray axisArray;
+
+    // Fetch data here
+    int xKey = newChartHeader.key( xAxisColumn );
+    int yKey = newChartHeader.key( yAxisColumn );
+
+    // Group name operations
+    QVector<int> groupKeyValues;
+    int groupKeySize = groupNames.length();
+
+    for(int i = 0; i < groupKeySize; i++){
+        groupKeyValues.append(newChartHeader.key(groupNames.at(i)));
+    }
+
+    int totalData = (*newChartData.value(xKey)).length();
+
+    for(int i = 0; i < totalData; i++){
+        QJsonObject axisData;
+        axisData.insert("name", (*newChartData.value(xKey)).at(i));
+        axisData.insert("size", (*newChartData.value(yKey)).at(i));
+        axisArray.append(axisData);
+
+    }
+
+    QJsonObject colData;
+    colData.insert("name", groupNames.at(0));
+    colData.insert("children", axisArray);
+
+    QJsonArray columns;
+    columns.append(xAxisColumn);
+    columns.append(yAxisColumn);
+
+    data.append(colData);
+    data.append(columns);
+
+    QJsonDocument doc;
+    doc.setArray(data);
+
+    QString strData = doc.toJson();
+
+    return strData;
 }
 
 void ChartsModel::getChartData(QMap<int, QStringList *> chartData)
