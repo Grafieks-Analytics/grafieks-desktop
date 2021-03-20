101950888ddf7158169041f6dbae3a61fb3d8738
Chilarai <chilly5476@gmail.com>
2021-03-20 22:36:35 +0530

    Test of sunburst. upto 2 columns and 1 row

--- Code/Logic/Reports/chartsmodel.cpp
+++ Code/Logic/Reports/chartsmodel.cpp
@@ -505,51 +505,178 @@ QString ChartsModel::getPivotChartValues(QString xAxisColumn, QString yAxisColum
     return "";
 }
 
-QString ChartsModel::getParentChildValues(QString xAxisColumn, QString yAxisColumn, QStringList groupNames)
+//QString ChartsModel::getParentChildValues(QStringList xAxisColumn, QString yAxisColumn)
+QString ChartsModel::getParentChildValues()
 {
+    qDebug() << "CLICKED ME";
+    QStringList xAxisColumn;
+    QString yAxisColumn;
+
+    xAxisColumn << "country" << "state" << "district";
+    yAxisColumn = "population";
 
     QJsonArray data;
     QJsonArray axisArray;
+    json output(json_array_arg);
+    QMap<QString, json> tmpJsonOutput;
+    json emptyJsonArray(json_array_arg);
+
+    // masterHash will be used to compare if any map has been generated earlier
+    // if there is an exact match with the hash, then it exists. Else create a new hash
+    QStringList *masterHash = new QStringList;
+
+    //
+    QMap<QString, long> *totalCount = new QMap<QString, long>;
 
     // Fetch data here
-    int xKey = newChartHeader.key( xAxisColumn );
+    int xKey = newChartHeader.key( xAxisColumn.at(0) );
     int yKey = newChartHeader.key( yAxisColumn );
 
     // Group name operations
     QVector<int> groupKeyValues;
-    int groupKeySize = groupNames.length();
+    int groupKeySize = xAxisColumn.length();
 
     for(int i = 0; i < groupKeySize; i++){
-        groupKeyValues.append(newChartHeader.key(groupNames.at(i)));
+        groupKeyValues.append(newChartHeader.key(xAxisColumn.at(i)));
     }
 
     int totalData = (*newChartData.value(xKey)).length();
 
-    for(int i = 0; i < totalData; i++){
-        QJsonObject axisData;
-        axisData.insert("name", (*newChartData.value(xKey)).at(i));
-        axisData.insert("size", (*newChartData.value(yKey)).at(i));
-        axisArray.append(axisData);
+    int yKeyLoop = 0;
+    QString paramName = "";
+    QString hashKeyword = "";
 
+    // Considering the measure as string here to avoid unwanted errors in wrong casting
+    // The front in javascript can easily handle this
+    long measure = 0;
+
+    //    for(int i = 0; i < totalData; i++){
+    for(int i = 0; i < 1; i++){
+
+        measure = (*newChartData.value(yKey)).at(i).toLong();
+
+        json tmpOutput;
+        QString pastHashKeyword;
+        json tmpJsonArray(json_array_arg);
+
+        for(int j = 0; j < groupKeySize; j++){
+
+            yKeyLoop = newChartHeader.key( xAxisColumn.at(j));
+            paramName = newChartData.value(yKeyLoop)->at(i);
+
+            // Generate unique hash to strings to be stored in master hash
+            if( j == 0){
+                hashKeyword.clear();
+                hashKeyword = paramName;
+            } else{
+                hashKeyword.append(paramName);
+            }
+
+            // If the hash doesnt exist, add to hash
+            if(!masterHash->contains(hashKeyword, Qt::CaseSensitive)){
+                masterHash->append(hashKeyword);
+                totalCount->insert(hashKeyword, measure);
+
+                // Check if first element of json is already there
+                // If not, then add it
+                if(j == 0){
+
+                    tmpOutput["name"] = paramName.toStdString();
+                    tmpOutput["size"] = measure;
+                    tmpOutput["children"] = emptyJsonArray;
+
+                    tmpJsonArray.push_back(tmpOutput);
+                    output.push_back(tmpOutput);
+                    tmpJsonOutput.insert(hashKeyword, tmpJsonArray);
+                    pastHashKeyword = hashKeyword;
+
+
+                } else{
+                    tmpOutput["name"] = paramName.toStdString();
+                    tmpOutput["size"] = measure;
+                    tmpOutput["children"] = emptyJsonArray;
+
+                    tmpJsonArray = tmpJsonOutput.value(pastHashKeyword).at(0).at("children");
+                    tmpJsonArray.push_back(tmpOutput);
+
+                    output.at(0).at("children").push_back(tmpOutput);
+                    tmpJsonOutput.insert(hashKeyword, tmpJsonArray);
+                    pastHashKeyword = hashKeyword;
+                }
+
+            } else{
+
+                //                long newValue = totalCount->value(hashKeyword) + measure;
+                //                totalCount->insert(hashKeyword, newValue);
+
+                //                if(tmpJsonOutput.contains(hashKeyword)){
+                //                    tmp = tmpJsonOutput.value(hashKeyword);
+                //                    qDebug() << hashKeyword << tmpJsonOutput.value(hashKeyword).to_string().c_str();
+                //                    tmp.push_back(newValue);
+                //                }
+            }
+
+        }
     }
 
-    QJsonObject colData;
-    colData.insert("name", groupNames.at(0));
-    colData.insert("children", axisArray);
+    qDebug() << output.to_string().c_str();
+    return "";
+}
 
-    QJsonArray columns;
-    columns.append(xAxisColumn);
-    columns.append(yAxisColumn);
+void ChartsModel::testing()
+{
+    qDebug() << "TESTING TRAIN";
+    // create an empty structure (null)
+    json j;
 
-    data.append(colData);
-    data.append(columns);
+    // add a number that is stored as double (note the implicit conversion of j to an object)
+    j["pi"] = 3.141;
 
-    QJsonDocument doc;
-    doc.setArray(data);
+    //    // add a Boolean that is stored as bool
+    j["happy"] = true;
 
-    QString strData = doc.toJson();
+    //    // add a string that is stored as std::string
+    j["name"] = "Niels";
 
-    return strData;
+    //    // add another null object by passing nullptr
+    //    j["nothing"] = nullptr;
+
+    //    // add an object inside the object
+    j["answer"]["everything"] = 42;
+    j["answer"]["everything2"]["a"] = 11;
+    //    j["answer"]["everything2"]["a"] = 21;
+    j["answer"]["everything2"]["b"]["b1"] = 2;
+    //    j["answer"]["everything2"]["b"]["b2"] = 3;
+
+    json a = json::make_array(2,0); // angle brackets can be omitted when N = 1
+    a[0] = j;
+    a[1] = j;
+
+    json b = a;
+
+    json color_spaces(json_array_arg); // an empty array
+    color_spaces.push_back(a);
+    color_spaces.push_back("AdobeRGB");
+    color_spaces.push_back("ProPhoto RGB");
+
+    json color_spaces1(json_array_arg); // an empty array
+    json x;
+    x["children"] = color_spaces1;
+
+    qDebug() <<x.at("children").to_string().c_str() << "CORO";
+    //    qDebug() <<a.to_string().c_str() << "TRAINING" << j.at("answer").at("everything").to_string().c_str() << j.contains("answer") << color_spaces.to_string().c_str();
+
+}
+
+void ChartsModel::finalTesting()
+{
+    json finale;
+
+    finale.make_array(5,0);
+    finale.push_back(1);
+    finale.push_back(2);
+    finale.push_back(3);
+    finale.push_back(4);
 }
 
 void ChartsModel::getChartData(QMap<int, QStringList *> chartData)
