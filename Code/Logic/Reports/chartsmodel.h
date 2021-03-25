91ca1c0a82a5163c264cf3d04c4e33b89dab0665
Chilarai <chilly5476@gmail.com>
2021-03-25 16:54:28 +0530

    Radar. Pie/Donut. Bugfixes

--- Code/Logic/Reports/chartsmodel.h
+++ Code/Logic/Reports/chartsmodel.h
@@ -24,18 +24,18 @@ public:
     Q_INVOKABLE QString getLineChartValues(QString xAxisColumn, QString yAxisColumn);
     Q_INVOKABLE QString getPieChartValues( QString xAxisColumn, QString yAxisColumn);
     Q_INVOKABLE QString getFunnelChartValues( QString xAxisColumn, QString yAxisColumn);
-    Q_INVOKABLE QString getRadarChartValues( QString xAxisColumn, QString yAxisColumn, QString yAxisCalculationType);
+    Q_INVOKABLE QString getRadarChartValues( QString xAxisColumn, QString yAxisColumn);
     Q_INVOKABLE QString getScatterChartValues( QString xAxisColumn, QString yAxisColumn, QString groupName);
     Q_INVOKABLE QString getHeatMapChartValues( QString xAxisColumn, QString yAxisColumn, QString groupName);
 
     Q_INVOKABLE QString getSunburstChartValues( QStringList xAxisColumn, QString yAxisColumn);
     Q_INVOKABLE QString getWaterfallChartValues( QString xAxisColumn, QString yAxisColumn, QStringList groupNames);
-    Q_INVOKABLE QString getGaugeChartValues(QString calculateColumn);
+    Q_INVOKABLE float getGaugeChartValues(QString calculateColumn);
     Q_INVOKABLE QString getSankeyChartValues( QString xAxisColumn, QString yAxisColumn, QStringList groupNames);
 
     Q_INVOKABLE QString getTreeChartValues( QStringList xAxisColumn, QString yAxisColumn);
     Q_INVOKABLE QString getTreeMapChartValues( QStringList xAxisColumn, QString yAxisColumn);
-    Q_INVOKABLE QString getKPIChartValues( QString calculateColumn);
+    Q_INVOKABLE float getKPIChartValues( QString calculateColumn);
     Q_INVOKABLE QString getTableChartValues( QString xAxisColumn, QString yAxisColumn, QStringList groupNames);
     Q_INVOKABLE QString getPivotChartValues( QString xAxisColumn, QString yAxisColumn, QStringList groupNames);
 
