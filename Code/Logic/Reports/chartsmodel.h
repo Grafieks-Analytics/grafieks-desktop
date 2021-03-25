2136475a82a5cdac66bd409ef01024b3aae9115d
Chilarai <chilly5476@gmail.com>
2021-03-25 21:54:59 +0530

    Scatter partial

--- Code/Logic/Reports/chartsmodel.h
+++ Code/Logic/Reports/chartsmodel.h
@@ -22,10 +22,11 @@ public:
     Q_INVOKABLE QString getGroupedBarChartValues(QString xAxisColumn, QString yAxisColumn, QString xSplitKey);
     Q_INVOKABLE QString getAreaChartValues(QString xAxisColumn, QString yAxisColumn);
     Q_INVOKABLE QString getLineChartValues(QString xAxisColumn, QString yAxisColumn);
+    Q_INVOKABLE QString getLineBarChartValues(QString xAxisColumn, QString yBarAxisColumn, QString yLineAxisColumn);
     Q_INVOKABLE QString getPieChartValues( QString xAxisColumn, QString yAxisColumn);
     Q_INVOKABLE QString getFunnelChartValues( QString xAxisColumn, QString yAxisColumn);
     Q_INVOKABLE QString getRadarChartValues( QString xAxisColumn, QString yAxisColumn);
-    Q_INVOKABLE QString getScatterChartValues( QString xAxisColumn, QString yAxisColumn, QString groupName);
+    Q_INVOKABLE QString getScatterChartValues( QString xAxisColumn, QString yAxisColumn, QString xSplitKey);
     Q_INVOKABLE QString getHeatMapChartValues( QString xAxisColumn, QString yAxisColumn, QString groupName);
 
     Q_INVOKABLE QString getSunburstChartValues( QStringList xAxisColumn, QString yAxisColumn);
