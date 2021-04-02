c386c0e0930428da34f469e8c159efa16346523a
Chilarai <chilly5476@gmail.com>
2021-04-02 16:33:57 +0530

    Duck second & successesive queris bugfix

--- Code/Logic/DataModeller/duckquerymodel.cpp
+++ Code/Logic/DataModeller/duckquerymodel.cpp
@@ -14,6 +14,7 @@ DuckQueryModel::DuckQueryModel(DuckCon *duckCon, QObject *parent)
 
 void DuckQueryModel::setQuery(QString query)
 {
+    this->resultData.clear();
     this->query = query;
     querySplitter.setQueryForClasses(this->query);
 
