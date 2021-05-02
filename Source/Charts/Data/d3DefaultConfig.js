5a00eec9ddf0bb7df53be5499af117b29b3c1104
abhishek3008 <abhisheksontakke92@gmail.com>
2021-05-03 01:27:39 +0530

    Scheme Value Changes

--- Source/Charts/Data/d3DefaultConfig.js
+++ Source/Charts/Data/d3DefaultConfig.js
@@ -9,7 +9,20 @@ const constants = {
 };
 
 const defaultD3Config = {
-    d3ColorPalette: "schemeSet3",
+    d3ColorPalette: [
+        "#8dd3c7",
+        "#ffffb3",
+        "#bebada",
+        "#fb8072",
+        "#80b1d3",
+        "#fdb462",
+        "#b3de69",
+        "#fccde5",
+        "#d9d9d9",
+        "#bc80bd",
+        "#ccebc5",
+        "#ffed6f",
+    ],
     defaultPaddingInner: 0.25,
     defaultMarkerShape: "circle",
     initialCircleRadius: 2,
