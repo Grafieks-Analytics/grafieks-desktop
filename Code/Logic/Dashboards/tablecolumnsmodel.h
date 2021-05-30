e93c105365ee31f10434f7df9dfbb54c4d7456b4
Chilarai <chilly5476@gmail.com>
2021-05-30 10:44:40 +0530

    Minor visual changes to dashbaoard filter. New grouped bar chart

--- Code/Logic/Dashboards/tablecolumnsmodel.h
+++ Code/Logic/Dashboards/tablecolumnsmodel.h
@@ -24,6 +24,7 @@ public:
 
     Q_INVOKABLE void setColumnVisibility(QString columnName, bool show = true);
     Q_INVOKABLE QStringList fetchVisibleColumns();
+//    Q_INVOKABLE void applyColumnVisibility(int dashboardId);
 
     Q_INVOKABLE QStringList fetchColumnData(QString colName);
     Q_INVOKABLE QStringList searchColumnData(QString keyword, QString columnName);
