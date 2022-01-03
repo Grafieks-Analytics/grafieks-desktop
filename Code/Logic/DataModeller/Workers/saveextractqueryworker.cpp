4fa077b4053c1b2d81ee40841eca361ed7ef0fbe
Chilarai <chilly5476@gmail.com>
2022-01-03 12:45:59 +0530

    Unwanted logs removed

--- Code/Logic/DataModeller/Workers/saveextractqueryworker.cpp
+++ Code/Logic/DataModeller/Workers/saveextractqueryworker.cpp
@@ -8,7 +8,6 @@ SaveExtractQueryWorker::SaveExtractQueryWorker(QString tmpSql, QVariantMap chang
 
 void SaveExtractQueryWorker::run()
 {
-    qDebug() << "EXTRACT SAVE CALLED";
     QString extractPath = Statics::extractPath;
     QString tableName = Statics::currentDbName;
 
