991951fa3bd2c559a5b17503b028be898c8d3734
Chilarai <chilly5476@gmail.com>
2022-02-17 16:47:28 +0530

    Sunburst tree and treemap

--- Code/Logic/General/chartsthread.h
+++ Code/Logic/General/chartsthread.h
@@ -105,6 +105,7 @@ public slots:
 private:
     duckdb::unique_ptr<duckdb::MaterializedQueryResult> queryExtractFunction(QString mainQuery);
     QSqlQuery queryLiveFunction(QString mainQuery);
+    QMap<int, QHash<int, QString>> queryLiveValues(QString mainQuery, int totalCols); // QHash instead of QStringList for faster access
     QString getTableName();
 
 signals:
