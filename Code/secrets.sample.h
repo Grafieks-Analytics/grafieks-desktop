ae615f4d75fd7a28cbb92f740a81096d6305edd4
Chilarai <chilly5476@gmail.com>
2022-12-28 23:16:31 +0530

    Secret sample file

---
+++ Code/secrets.sample.h
@@ -0,0 +1,42 @@
+#ifndef SECRETS_SAMPLE_H
+#define SECRETS_SAMPLE_H
+
+#include <QString>
+
+//namespace Secret
+//{
+
+//    // DropBox
+//    const QString dropBoxClient = "xxx"; // Replace it with your Dropbox client
+//    const QString dropBoxSecret = "xxx"; // Replace it with your Dropbox secret
+//    const int dropBoxPort = 5477;
+
+//    // Google Drive
+//    const QString driveClient = "xxx"; // Replace it with your Google drive client
+//    const QString driveSecret = "xxx"; // Replace it with your Google drive secret
+//    const int drivePort = 5478;
+
+//    // Google Sheet
+//    const QString sheetClient = "xxx"; // Replace it with your Google drive client
+//    const QString sheetSecret = "xxx"; // Replace it with your Google drive secret
+//     const int sheetPort = 5479;
+
+//    // Box
+//    const QString boxClient = "xxx"; // Replace it with your Box client
+//    const QString boxSecret = "xxx"; // Replace it with your Box secret
+//    const int boxPort = 5480;
+
+//    // Github
+//    const QString githubClient = "xxx"; // Replace it with your Github client
+//    const QString githubSecret = "xxx"; // Replace it with your Github secret
+//    const int githubPort = 5481;
+
+//    // SimpleCrypt
+//    const int simpleCryptHash = 112233; // Replace it with your own hash
+
+
+//    // Bugsplat
+//    const QString bugSplatEmail = "email@email.com";
+//}
+
+#endif // SECRETS_SAMPLE_H
